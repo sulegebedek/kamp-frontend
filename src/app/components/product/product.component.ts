45158ad563ee74ee852fa6744a0a5833dcd4b288
sulegebedek <sulegebedek@gmail.com>
2021-03-28 03:21:49 +0300

    kamp-frontend

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,47 +13,44 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  filterText:"";
+  filterText="";
   
-  constructor(private productService:ProductService,
+  constructor(private productService:ProductService, 
     private activatedRoute:ActivatedRoute, 
     private toastrService:ToastrService,
-    private cartservice:CartService) {}
+    private cartService:CartService) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
-        this.getProductsByCategory(params["categoryId"])
-      }
-      else{
-        this.getProducts()
-      }
-    })
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductsByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
   }
 
   getProducts() {
     this.productService.getProducts().subscribe(response=>{
       this.products = response.data
       this.dataLoaded = true;
-    })
-    
+    })   
   }
 
   getProductsByCategory(categoryId:number) {
     this.productService.getProductsByCategory(categoryId).subscribe(response=>{
       this.products = response.data
       this.dataLoaded = true;
-    })
-    
+    })   
   }
 
   addToCart(product:Product){
-    if(product.unitsInStock === 0){
-      this.toastrService.error("Stokta bu üründen bulunamadığı için sepete eklenemedi.")
-    }
-    else{
-      this.toastrService.success("Sepete Eklendi",product.productName)
-      this.cartservice.addToCart(product);
-    }
+      if(product.unitsInStock===0){
+        this.toastrService.error("Hata","Stokta bu üründen bulunamadığı için sepete eklenemedi")
+      }else{
+        this.toastrService.success("Sepete eklendi",product.productName)
+        this.cartService.addToCart(product);
+      }
+   
   }
 }
